e08c66060130d1725abea3b91d433e2f8281c583
deathfisaro <deathfisaro@gmail.com>
2014-06-10 17:05:05 -0700

    Mass update
    ItemDex.sql: ItemID now auto_incremented instead
    Project.sql: VARCHAR(15) increased to 40 as some strings were truncated
    Towns.sql: comments -- -> #
    Trainers.sql: INSERT format fixed to properly insert values

--- itemdex.sql
+++ itemdex.sql
@@ -1,215 +1,215 @@
-insert into ItemDex
-values(0, 'Antidote', 'Cures poison.');
+insert into ItemDex(itemName, description)
+values('Antidote', 'Cures poison.');
 
-insert into ItemDex
-values(1, 'Awakening', 'Cures sleep.');
+insert into ItemDex(itemName, description)
+values('Awakening', 'Cures sleep.');
 
-insert into ItemDex
-values(2, 'Bicycle', 'Allows you to travel faster.');
+insert into ItemDex(itemName, description)
+values('Bicycle', 'Allows you to travel faster.');
 
-insert into ItemDex
-values(3, 'Bike Voucher', 'Used to get a FREE Bike.');
+insert into ItemDex(itemName, description)
+values('Bike Voucher', 'Used to get a FREE Bike.');
 
-insert into ItemDex
-values(4, 'Burn Heal', 'Heals burns.');
+insert into ItemDex(itemName, description)
+values('Burn Heal', 'Heals burns.');
 
-insert into ItemDex
-values(5, 'Calcium', 'Increases Special level.');
+insert into ItemDex(itemName, description)
+values('Calcium', 'Increases Special level.');
 
-insert into ItemDex
-values(6, 'Carbos', 'Increases Speed level.');
+insert into ItemDex(itemName, description)
+values('Carbos', 'Increases Speed level.');
 
-insert into ItemDex
-values(7, 'Card Key', 'Unlocks Silph Co. doors.');
+insert into ItemDex(itemName, description)
+values('Card Key', 'Unlocks Silph Co. doors.');
 
-insert into ItemDex
-values(8, 'Coin', 'Slot Machine money.');
+insert into ItemDex(itemName, description)
+values('Coin', 'Slot Machine money.');
 
-insert into ItemDex
-values(9, 'Coin Case', 'Holds coins.');
+insert into ItemDex(itemName, description)
+values('Coin Case', 'Holds coins.');
 
-insert into ItemDex
-values(10, 'Dire Hit', 'Increases attack effectiveness.');
+insert into ItemDex(itemName, description)
+values('Dire Hit', 'Increases attack effectiveness.');
 
-insert into ItemDex
-values(11, 'Dome Fossil', 'Changes into Kabuto.');
+insert into ItemDex(itemName, description)
+values('Dome Fossil', 'Changes into Kabuto.');
 
-insert into ItemDex
-values(12, 'Elixer', 'Gives 10 PP to all Attacks.');
+insert into ItemDex(itemName, description)
+values('Elixer', 'Gives  PP to all Attacks.');
 
-insert into ItemDex
-values(13, 'Escape Rope', 'Used to escape an area.');
+insert into ItemDex(itemName, description)
+values('Escape Rope', 'Used to escape an area.');
 
-insert into ItemDex
-values(14, 'Ether', 'Gives 10 PP to one Ability.');
+insert into ItemDex(itemName, description)
+values('Ether', 'Gives  PP to one Ability.');
 
-insert into ItemDex
-values(15, 'Exp. All', 'Shares experience points.');
+insert into ItemDex(itemName, description)
+values('Exp. All', 'Shares experience points.');
 
-insert into ItemDex
-values(16, 'Fire Stone', 'Evolves some Fire Pokemon.');
+insert into ItemDex(itemName, description)
+values('Fire Stone', 'Evolves some Fire Pokemon.');
 
-insert into ItemDex
-values(17, 'Fresh Water', 'Quenches thirst. Restores 50 HP.');
+insert into ItemDex(itemName, description)
+values('Fresh Water', 'Quenches thirst. Restores  HP.');
 
-insert into ItemDex
-values(18, 'Full Heal', 'Cures any ailments.');
+insert into ItemDex(itemName, description)
+values('Full Heal', 'Cures any ailments.');
 
-insert into ItemDex
-values(19, 'Full Restore', 'Cures any ailments. Restores all HP.');
+insert into ItemDex(itemName, description)
+values('Full Restore', 'Cures any ailments. Restores all HP.');
 
-insert into ItemDex
-values(20, 'Gold Teeth', 'Helps Warden to Speak.');
+insert into ItemDex(itemName, description)
+values('Gold Teeth', 'Helps Warden to Speak.');
 
-insert into ItemDex
-values(21, 'Good Rod', 'Used to fish.');
+insert into ItemDex(itemName, description)
+values('Good Rod', 'Used to fish.');
 
-insert into ItemDex
-values(22, 'Great Ball', 'More effective than a Pokeball.');
+insert into ItemDex(itemName, description)
+values('Great Ball', 'More effective than a Pokeball.');
 
-insert into ItemDex
-values(23, 'Guard Spec.', 'Disables Special Attacks.');
+insert into ItemDex(itemName, description)
+values('Guard Spec.', 'Disables Special Attacks.');
 
-insert into ItemDex
-values(24, 'Helix Fossil', 'Changes into Omanyte.');
+insert into ItemDex(itemName, description)
+values('Helix Fossil', 'Changes into Omanyte.');
 
-insert into ItemDex
-values(25, 'HP Up', 'Increases HP by 1.');
+insert into ItemDex(itemName, description)
+values('HP Up', 'Increases HP by .');
 
-insert into ItemDex
-values(26, 'Hyper Potion', 'Restores 200 HP.');
+insert into ItemDex(itemName, description)
+values('Hyper Potion', 'Restores  HP.');
 
-insert into ItemDex
-values(27, 'Ice Heal', 'Heals frozen Pokemon.');
+insert into ItemDex(itemName, description)
+values('Ice Heal', 'Heals frozen Pokemon.');
 
-insert into ItemDex
-values(28, 'Iron', 'Increases Defence level.');
+insert into ItemDex(itemName, description)
+values('Iron', 'Increases Defence level.');
 
-insert into ItemDex
-values(29, 'Item Finder', 'Finds Hidden Items.');
+insert into ItemDex(itemName, description)
+values('Item Finder', 'Finds Hidden Items.');
 
-insert into ItemDex
-values(30, 'Leaf Stone', 'Evolves some Grass Pokemon');
+insert into ItemDex(itemName, description)
+values('Leaf Stone', 'Evolves some Grass Pokemon');
 
-insert into ItemDex
-values(31, 'Lemonade', 'Quenches thirst. Restores 80 HP.');
+insert into ItemDex(itemName, description)
+values('Lemonade', 'Quenches thirst. Restores  HP.');
 
-insert into ItemDex
-values(32, 'Lift Key', 'Activates Game Corner Elevator.');
+insert into ItemDex(itemName, description)
+values('Lift Key', 'Activates Game Corner Elevator.');
 
-insert into ItemDex
-values(33, 'Master Ball', 'Catches Pokemon 100% of time.');
+insert into ItemDex(itemName, description)
+values('Master Ball', 'Catches Pokemon % of time.');
 
-insert into ItemDex
-values(34, 'Max Elixer', 'Restores all PP.');
+insert into ItemDex(itemName, description)
+values('Max Elixer', 'Restores all PP.');
 
-insert into ItemDex
-values(35, 'Max Ether', 'Restores all PP of 1 ability.');
+insert into ItemDex(itemName, description)
+values('Max Ether', 'Restores all PP of  ability.');
 
-insert into ItemDex
-values(36, 'Max Potion', 'Restores all HP.');
+insert into ItemDex(itemName, description)
+values('Max Potion', 'Restores all HP.');
 
-insert into ItemDex
-values(37, 'Max Repel', 'Prevents wild Pokemon from attacking.');
+insert into ItemDex(itemName, description)
+values('Max Repel', 'Prevents wild Pokemon from attacking.');
 
-insert into ItemDex
-values(38, 'Max Revive', 'Revives. Restores all HP.');
+insert into ItemDex(itemName, description)
+values('Max Revive', 'Revives. Restores all HP.');
 
-insert into ItemDex
-values(39, 'Moon Stone', 'Evolves some Pokemon.');
+insert into ItemDex(itemName, description)
+values('Moon Stone', 'Evolves some Pokemon.');
 
-insert into ItemDex
-values(40, 'Nugget', 'Sold for $5000.');
+insert into ItemDex(itemName, description)
+values('Nugget', 'Sold for $.');
 
-insert into ItemDex
-values(41, 'Oaks Parcel', 'Exchange for Pokedex.');
+insert into ItemDex(itemName, description)
+values('Oaks Parcel', 'Exchange for Pokedex.');
 
-insert into ItemDex
-values(42, 'Old Amber', 'Changes into Aerodactyl.');
+insert into ItemDex(itemName, description)
+values('Old Amber', 'Changes into Aerodactyl.');
 
-insert into ItemDex
-values(43, 'Old Rod', 'Fish for Magikarp.');
+insert into ItemDex(itemName, description)
+values('Old Rod', 'Fish for Magikarp.');
 
-insert into ItemDex
-values(44, 'Paralyze Heal', 'Heals Paralysis.');
+insert into ItemDex(itemName, description)
+values('Paralyze Heal', 'Heals Paralysis.');
 
-insert into ItemDex
-values(45, 'Pokeball', 'Lowest odds of catching Pokemon.');
+insert into ItemDex(itemName, description)
+values('Pokeball', 'Lowest odds of catching Pokemon.');
 
-insert into ItemDex
-values(46, 'Pokedex', 'Records Pokemon Data.');
+insert into ItemDex(itemName, description)
+values('Pokedex', 'Records Pokemon Data.');
 
-insert into ItemDex
-values(47, 'Pokedoll', 'Distracts Opponents. Makes girls happy.');
+insert into ItemDex(itemName, description)
+values('Pokedoll', 'Distracts Opponents. Makes girls happy.');
 
-insert into ItemDex
-values(48, 'Pokeflute', 'Wakes sleeping Pokemon.');
+insert into ItemDex(itemName, description)
+values('Pokeflute', 'Wakes sleeping Pokemon.');
 
-insert into ItemDex
-values(49, 'Potion', 'Restores 20 HP.');
+insert into ItemDex(itemName, description)
+values('Potion', 'Restores  HP.');
 
-insert into ItemDex
-values(50, 'PP Up', 'Increases PP by 1.');
+insert into ItemDex(itemName, description)
+values('PP Up', 'Increases PP by .');
 
-insert into ItemDex
-values(51, 'Protein', 'Increases Attack level.');
+insert into ItemDex(itemName, description)
+values('Protein', 'Increases Attack level.');
 
-insert into ItemDex
-values(52, 'Rare Candy', 'Increases Exp Level by 1.');
+insert into ItemDex(itemName, description)
+values('Rare Candy', 'Increases Exp Level by .');
 
-insert into ItemDex
-values(53, 'Repel', 'Prevents wild Pokemon from attacking.');
+insert into ItemDex(itemName, description)
+values('Repel', 'Prevents wild Pokemon from attacking.');
 
-insert into ItemDex
-values(54, 'Revive', 'Revives fainted Pokemon.');
+insert into ItemDex(itemName, description)
+values('Revive', 'Revives fainted Pokemon.');
 
-insert into ItemDex
-values(55, 'S.S. Ticket', 'Used to Board S.S. Anne.');
+insert into ItemDex(itemName, description)
+values('S.S. Ticket', 'Used to Board S.S. Anne.');
 
-insert into ItemDex
-values(56, 'Safari Ball', 'Used in Safari Zone.');
+insert into ItemDex(itemName, description)
+values('Safari Ball', 'Used in Safari Zone.');
 
-insert into ItemDex
-values(57, 'Secret Key', 'Unlocks Cinnabar Island Gym.');
+insert into ItemDex(itemName, description)
+values('Secret Key', 'Unlocks Cinnabar Island Gym.');
 
-insert into ItemDex
-values(58, 'Silph Scope', 'Used to identify Ghosts.');
+insert into ItemDex(itemName, description)
+values('Silph Scope', 'Used to identify Ghosts.');
 
-insert into ItemDex
-values(59, 'Soda Pop', 'Quenches thirst. Restores 60 HP.');
+insert into ItemDex(itemName, description)
+values('Soda Pop', 'Quenches thirst. Restores  HP.');
 
-insert into ItemDex
-values(60, 'Super Potion', 'Restores 50 HP.');
+insert into ItemDex(itemName, description)
+values('Super Potion', 'Restores  HP.');
 
-insert into ItemDex
-values(61, 'Super Repel', 'Prevents wild Pokemon from attacking.');
+insert into ItemDex(itemName, description)
+values('Super Repel', 'Prevents wild Pokemon from attacking.');
 
-insert into ItemDex
-values(62, 'Super Rod', 'Fishes for Pokemon.');
+insert into ItemDex(itemName, description)
+values('Super Rod', 'Fishes for Pokemon.');
 
-insert into ItemDex
-values(63, 'Thunder Stone', 'Evolves Pikachu.');
+insert into ItemDex(itemName, description)
+values('Thunder Stone', 'Evolves Pikachu.');
 
-insert into ItemDex
-values(64, 'Town Map', 'Shows you where you are.');
+insert into ItemDex(itemName, description)
+values('Town Map', 'Shows you where you are.');
 
-insert into ItemDex
-values(65, 'Ultra Ball', 'Better than Great Balls.');
+insert into ItemDex(itemName, description)
+values('Ultra Ball', 'Better than Great Balls.');
 
-insert into ItemDex
-values(66, 'Water Stone', 'Evolves some water Pokemon.');
+insert into ItemDex(itemName, description)
+values('Water Stone', 'Evolves some water Pokemon.');
 
-insert into ItemDex
-values(67, 'X Accuracy', 'Boosts Accuracy Temporarily.');
+insert into ItemDex(itemName, description)
+values('X Accuracy', 'Boosts Accuracy Temporarily.');
 
-insert into ItemDex
-values(68, 'X Attack', 'Boosts Attack Temporarily.');
+insert into ItemDex(itemName, description)
+values('X Attack', 'Boosts Attack Temporarily.');
 
-insert into ItemDex
-values(69, 'X Defend', 'Boosts Defence Temporarily.');
+insert into ItemDex(itemName, description)
+values('X Defend', 'Boosts Defence Temporarily.');
 
-insert into ItemDex
-values(70, 'X Special', 'Boosts Special Temporarily.');
+insert into ItemDex(itemName, description)
+values('X Special', 'Boosts Special Temporarily.');
 
-insert into ItemDex
-values(71, 'X Speed', 'Boosts Speed Temporarily.');
+insert into ItemDex(itemName, description)
+values('X Speed', 'Boosts Speed Temporarily.');
--- trainers.sql
+++ trainers.sql
@@ -1,124 +1,124 @@
---Since we are using auto-increment there is no trainer id.
+#Since we are using auto-increment there is no trainer id.
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Robert', 'Pallet Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Gary', 'Pallet Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Ash', 'Viridian City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('May', 'Viridian City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Josh', 'Pewter City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Henry', 'Pewter City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Ken', 'Cerulean City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Cassandra', 'Cerulean City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Mike', 'Vermilion City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Jenny', 'Vermilion City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Amanda', 'Lavender Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Thomas', 'Lavender Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Wesley', 'Celadon City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Leon', 'Fuchsia City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Shawn', 'Saffron City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Lina', 'Cinnabar Island');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Peter', 'New Bark Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Lois', 'Cherrygrove City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Stewie', 'Violet City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Brian', 'Azalea Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Christopher', 'Goldenrod City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Megan', 'Ecruteak City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Stan', 'Olivine City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Kyle', 'Mahogany Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Eric', 'Blackthorn City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Kenny', 'Littleroot Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Craig', 'Oldale Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Butters', 'Petalburg City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Rylai', 'Rustboro City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Davion', 'Dewford Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Boush', 'Slateport City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Kael', 'Maulville City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Akasha', 'Verdanturf Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Lyralei', 'Fallarbor Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Leoric', 'Lavaridge Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Jeffery', 'Fortree City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Jackson', 'Lilycove City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Wayne', 'Mossdeep City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Shaq', 'Sootopolis City');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Henrik', 'Pacifidlog Town');
 
-insert into Trainers
+insert into Trainers(trainerName, townName)
 value ('Daniel', 'Ever Grand City');
