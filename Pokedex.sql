dbb3ea26ef60bbc4cac9b6de00d61aeea355f2e8
Cassandra Lim <cassandralim.tmd@gmail.com>
2014-06-11 11:18:26 -0700

    Pokedex
    Added pokedex file

---
+++ Pokedex.sql
@@ -0,0 +1,329 @@
+--Populating Pokedex table
+--PokeDex (dexNo, species, type, gender)
+
+
+-- dexNo: From 1-151
+-- species: String representing pokemon names
+-- type: Consists of any 1 or 2 Grass, Poison, Fire, Water, Bug, Flying, Normal, Electric, 
+-- Ground, Rock, Fairy, Psychic, Fighting, Dragon, Ice, Ghost
+
+
+insert into Pokedex
+values('001', 'Bulbasaur', 'Grass Poison', 'MF');
+insert into Pokedex
+values('002', 'Ivysaur', 'Grass Poison', 'MF');
+insert into Pokedex
+values('003', 'Venusaur', 'Grass Poison', 'MF');
+insert into Pokedex
+values('004', 'Charmander', 'Fire', 'MF');
+insert into Pokedex
+values('005', 'Charmeleon', 'Fire', 'MF');
+insert into Pokedex
+values('006', 'Charizard', 'Fire Flying', 'MF');
+insert into Pokedex
+values('007', 'Squirtle', 'Water', 'MF');
+insert into Pokedex
+values('008', 'Wartortle', 'Water', 'MF');
+insert into Pokedex
+values('009', 'Blastoise', 'Water', 'MF');
+insert into Pokedex
+values('010', 'Caterpie', 'Bug', 'MF');
+
+insert into Pokedex
+values('011', 'Metapod', 'Bug', 'MF');
+insert into Pokedex
+values('012', 'Butterfree', 'Bug Flying', 'MF');
+insert into Pokedex
+values('013', 'Weedle', 'Bug Poison', 'MF');
+insert into Pokedex
+values('014', 'Kakuna', 'Bug Poison', 'MF');
+insert into Pokedex
+values('015', 'Beedrill', 'Bug Poison', 'MF');
+insert into Pokedex
+values('016', 'Pidgey', 'Normal Flying', 'MF');
+insert into Pokedex
+values('017', 'Pidgeotto', 'Normal Flying', 'MF');
+insert into Pokedex
+values('018', 'Pidgeot', 'Normal Flying', 'MF');
+insert into Pokedex
+values('019', 'Rattata', 'Normal', 'MF');
+insert into Pokedex
+values('020', 'Raticate', 'Normal', 'MF');
+
+insert into Pokedex
+values('021', 'Spearow', 'Normal Flying', 'MF');
+insert into Pokedex
+values('022', 'Fearow', 'Normal Flying', 'MF');
+insert into Pokedex
+values('023', 'Ekans', 'Poison', 'MF');
+insert into Pokedex
+values('024', 'Arbok', 'Poison', 'MF');
+insert into Pokedex
+values('025', 'Pikachu', 'Electric', 'MF');
+insert into Pokedex
+values('026', 'Raichu', 'Electric', 'MF');
+insert into Pokedex
+values('027', 'Sandshrew', 'Ground', 'MF');
+insert into Pokedex
+values('028', 'Sandslash', 'Ground', 'MF');
+insert into Pokedex
+values('029', 'NidoranF', 'Poison', 'F');
+insert into Pokedex
+values('030', 'Nidorina', 'Poison', 'F');
+
+insert into Pokedex
+values('031', 'Nidoqueen', 'Poison Ground', 'F');
+insert into Pokedex
+values('032', 'NidoranM', 'Poison', 'M');
+insert into Pokedex
+values('033', 'Nidorino', 'Poison', 'M');
+insert into Pokedex
+values('034', 'Nidoking', 'Poison Ground', 'F');
+insert into Pokedex
+values('035', 'Clefairy', 'Fairy', 'MF');
+insert into Pokedex
+values('036', 'Clefable', 'Fairy', 'MF');
+insert into Pokedex
+values('037', 'Vulpix', 'Fire', 'MF');
+insert into Pokedex
+values('038', 'Ninetails', 'Fire', 'MF');
+insert into Pokedex
+values('039', 'Jigglypuff', 'Normal Fairy', 'MF');
+insert into Pokedex
+values('040', 'Wigglytuff', 'Normal Fairy', 'MF');
+
+insert into Pokedex
+values('041', 'Zubat', 'Poison Flying', 'MF');
+insert into Pokedex
+values('042', 'Golbat', 'Poison Flying', 'MF');
+insert into Pokedex
+values('043', 'Oddish', 'Grass Poison', 'MF');
+insert into Pokedex
+values('044', 'Gloom', 'Grass Poison', 'MF');
+insert into Pokedex
+values('045', 'Vileplume', 'Grass Poison', 'MF');
+insert into Pokedex
+values('046', 'Paras', 'Bug Grass', 'MF');
+insert into Pokedex
+values('047', 'Parasect', 'Bug Grass', 'MF');
+insert into Pokedex
+values('048', 'Venonat', 'Bug Poison', 'MF');
+insert into Pokedex
+values('049', 'Venomoth', 'Bug Poison', 'MF');
+insert into Pokedex
+values('050', 'Diglett', 'Ground', 'MF');
+
+insert into Pokedex
+values('051', 'Dugtrio', 'Ground', 'MF');
+insert into Pokedex
+values('052', 'Meowth', 'Normal', 'MF');
+insert into Pokedex
+values('053', 'Persian', 'Normal', 'MF');
+insert into Pokedex
+values('054', 'Psyduck', 'Water', 'MF');
+insert into Pokedex
+values('055', 'Golduck', 'Water', 'MF');
+insert into Pokedex
+values('056', 'Mankey', 'Fighting', 'MF');
+insert into Pokedex
+values('057', 'Primeape', 'Fighting', 'MF');
+insert into Pokedex
+values('058', 'Growlithe', 'Fire', 'MF');
+insert into Pokedex
+values('059', 'Arcanine', 'Fire', 'MF');
+insert into Pokedex
+values('060', 'Poliwag', 'Water', 'MF');
+
+insert into Pokedex
+values('061', 'Poliwhirl','Water', 'MF');
+insert into Pokedex
+values('062', 'Poliwrath', 'Water Fighting', 'MF');
+insert into Pokedex
+values('063', 'Abra', 'Psychic', 'MF');
+insert into Pokedex
+values('064', 'Kadabra', 'Psychic', 'MF');
+insert into Pokedex
+values('065', 'Alakazam', 'Psychic', 'MF');
+insert into Pokedex
+values('066', 'Machop', 'Fighting', 'MF');
+insert into Pokedex
+values('067', 'Machoke', 'Fighting', 'MF');
+insert into Pokedex
+values('068', 'Machamp', 'Fighting', 'MF');
+insert into Pokedex
+values('069', 'Bellsprout', 'Grass Poison', 'MF');
+insert into Pokedex
+values('070', 'Weepinbell', 'Grass Poison', 'MF');
+
+insert into Pokedex
+values('071', 'Victreebel', 'Grass Poison', 'MF');
+insert into Pokedex
+values('072', 'Tentacool', 'Water Poison', 'MF');
+insert into Pokedex
+values('073', 'Tentacruel', 'Water Poison', 'MF');
+insert into Pokedex
+values('074', 'Geodude', 'Rock Ground', 'MF');
+insert into Pokedex
+values('075', 'Graveler', 'Rock Ground', 'MF');
+insert into Pokedex
+values('076', 'Golem', 'Rock Ground', 'MF');
+insert into Pokedex
+values('077', 'Ponyta', 'Fire', 'MF');
+insert into Pokedex
+values('078', 'Rapidash', 'Fire', 'MF');
+insert into Pokedex
+values('079', 'Slowpoke', 'Water Psychic', 'MF');
+insert into Pokedex
+values('080', 'Slowbro', 'Water Psychic', 'MF');
+
+insert into Pokedex
+values('081', 'Magnemite', 'Electric Steel', 'U');
+insert into Pokedex
+values('082', 'Magneton', 'Electric Steel', 'U');
+insert into Pokedex
+values('083', 'Farfetch\'d', 'Normal Flying', 'MF');
+insert into Pokedex
+values('084', 'Doduo', 'Normal Flying', 'MF');
+insert into Pokedex
+values('085', 'Dodrio', 'Normal Flying', 'MF');
+insert into Pokedex
+values('086', 'Seel', 'Water', 'MF');
+insert into Pokedex
+values('087', 'Dewgong', 'Water Ice', 'MF');
+insert into Pokedex
+values('088', 'Grimer', 'Poison', 'MF');
+insert into Pokedex
+values('089', 'Muk', 'Poison', 'MF');
+insert into Pokedex
+values('090', 'Shellder', 'Water', 'MF');
+
+insert into Pokedex
+values('091', 'Cloyster', 'Water Ice', 'MF');
+insert into Pokedex
+values('092', 'Gastly', 'Ghost Poison', 'MF');
+insert into Pokedex
+values('093', 'Haunter', 'Ghost Poison', 'MF');
+insert into Pokedex
+values('094', 'Gengar', 'Ghost Poison', 'MF');
+insert into Pokedex
+values('095', 'Onix', 'Rock Ground', 'MF');
+insert into Pokedex
+values('096', 'Drowzee', 'Psychic', 'MF');
+insert into Pokedex
+values('097', 'Hypno', 'Psychic', 'MF');
+insert into Pokedex
+values('098', 'Krabby', 'Water', 'MF');
+insert into Pokedex
+values('099', 'Kingler', 'Water', 'MF');
+insert into Pokedex
+values('100', 'Voltorb', 'Electric', 'U');
+
+insert into Pokedex
+values('101', 'Electrode', 'Electric', 'U');
+insert into Pokedex
+values('102', 'Exeggcute', 'Grass Psychic', 'MF');
+insert into Pokedex
+values('103', 'Exeggutor', 'Grass Psychic', 'MF');
+insert into Pokedex
+values('104', 'Cubone', 'Ground', 'MF');
+insert into Pokedex
+values('105', 'Marowak', 'Ground', 'MF');
+insert into Pokedex
+values('106', 'Hitmonlee', 'Fighting', 'M');
+insert into Pokedex
+values('107', 'Hitmonchan', 'Fighting', 'M');
+insert into Pokedex
+values('108', 'Lickitung', 'Normal', 'MF');
+insert into Pokedex
+values('109', 'Koffing', 'Poison', 'MF');
+insert into Pokedex
+values('110', 'Weezing', 'Poison', 'MF');
+
+insert into Pokedex
+values('111', 'Rhyhorn', 'Ground Rock', 'MF');
+insert into Pokedex
+values('112', 'Rhydon', 'Ground Rock', 'MF');
+insert into Pokedex
+values('113', 'Chansey', 'Normal', 'F');
+insert into Pokedex
+values('114', 'Tangela', 'Grass', 'MF');
+insert into Pokedex
+values('115', 'Kangaskhan', 'Normal', 'F');
+insert into Pokedex
+values('116', 'Horsea', 'Water', 'MF');
+insert into Pokedex
+values('117', 'Seadra', 'Water', 'MF');
+insert into Pokedex
+values('118', 'Goldeen', 'Water', 'MF');
+insert into Pokedex
+values('119', 'Seaking', 'Water', 'MF');
+insert into Pokedex
+values('120', 'Staryu', 'Water', 'U');
+
+insert into Pokedex
+values('121', 'Starmie', 'Water Psychic', 'U');
+insert into Pokedex
+values('122', 'Mr. Mime', 'Psychic Fairy', 'MF');
+insert into Pokedex
+values('123', 'Scyther', 'Bug Flying', 'MF');
+insert into Pokedex
+values('124', 'Jynx', 'Ice Psychic', 'F');
+insert into Pokedex
+values('125', 'Electabuzz', 'Electric', 'MF');
+insert into Pokedex
+values('126', 'Magmar', 'Fire', 'MF');
+insert into Pokedex
+values('127', 'Pinsir', 'Bug', 'MF');
+insert into Pokedex
+values('128', 'Tauros', 'Normal', 'M');
+insert into Pokedex
+values('129', 'Magikarp', 'Water', 'MF');
+insert into Pokedex
+values('130', 'Gyarados', 'Water Flying', 'MF');
+
+insert into Pokedex
+values('131', 'Lapras', 'Water Ice', 'MF');
+insert into Pokedex
+values('132', 'Ditto', 'Normal', 'U');
+insert into Pokedex
+values('133', 'Eevee', 'Normal', 'MF');
+insert into Pokedex
+values('134', 'Vaporeon', 'Water', 'MF');
+insert into Pokedex
+values('135', 'Jolteon', 'Electric', 'MF');
+insert into Pokedex
+values('136', 'Flareon', 'Fire', 'MF');
+insert into Pokedex
+values('137', 'Porygon', 'Normal', 'U');
+insert into Pokedex
+values('138', 'Omanyte', 'Rock Water', 'MF');
+insert into Pokedex
+values('139', 'Omastar', 'Rock Water', 'MF');
+insert into Pokedex
+values('140', 'Kabuto', 'Rock Water', 'MF');
+
+insert into Pokedex
+values('141', 'Kabutops', 'Rock Water', 'MF');
+insert into Pokedex
+values('142', 'Aerodactyl', 'Rock Flying', 'MF');
+insert into Pokedex
+values('143', 'Snorlax', 'Normal', 'MF');
+insert into Pokedex
+values('144', 'Articuno', 'Ice Flying', 'U');
+insert into Pokedex
+values('145', 'Zapdos', 'Electric Flying', 'U');
+insert into Pokedex
+values('146', 'Moltres', 'Fire Flying', 'U');
+insert into Pokedex
+values('147', 'Dratini', 'Dragon', 'MF');
+insert into Pokedex
+values('148', 'Dragonair', 'Dragon', 'MF');
+insert into Pokedex
+values('149', 'Dragonite', 'Dragon Flying', 'MF');
+insert into Pokedex
+values('150', 'Mewtwo', 'Psychic', 'U');
+insert into Pokedex
+values('151', 'Mew', 'Psychic', 'U');
+
+
+
