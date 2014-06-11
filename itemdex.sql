9d63687b51b9b6a9108497a8b657e464445a6c20
HenryLoo <henry_loo@live.ca>
2014-06-11 09:09:00 -0700

    ItemDex Update
    Removed itemIDs from inserts because of auto increment.

--- itemdex.sql
+++ itemdex.sql
@@ -1,215 +1,215 @@
 insert into ItemDex
-values(0, 'Antidote', 'Cures poison.');
+values('Antidote', 'Cures poison.');
 
 insert into ItemDex
-values(1, 'Awakening', 'Cures sleep.');
+values('Awakening', 'Cures sleep.');
 
 insert into ItemDex
-values(2, 'Bicycle', 'Allows you to travel faster.');
+values('Bicycle', 'Allows you to travel faster.');
 
 insert into ItemDex
-values(3, 'Bike Voucher', 'Used to get a FREE Bike.');
+values('Bike Voucher', 'Used to get a FREE Bike.');
 
 insert into ItemDex
-values(4, 'Burn Heal', 'Heals burns.');
+values('Burn Heal', 'Heals burns.');
 
 insert into ItemDex
-values(5, 'Calcium', 'Increases Special level.');
+values('Calcium', 'Increases Special level.');
 
 insert into ItemDex
-values(6, 'Carbos', 'Increases Speed level.');
+values('Carbos', 'Increases Speed level.');
 
 insert into ItemDex
-values(7, 'Card Key', 'Unlocks Silph Co. doors.');
+values('Card Key', 'Unlocks Silph Co. doors.');
 
 insert into ItemDex
-values(8, 'Coin', 'Slot Machine money.');
+values('Coin', 'Slot Machine money.');
 
 insert into ItemDex
-values(9, 'Coin Case', 'Holds coins.');
+values('Coin Case', 'Holds coins.');
 
 insert into ItemDex
-values(10, 'Dire Hit', 'Increases attack effectiveness.');
+values('Dire Hit', 'Increases attack effectiveness.');
 
 insert into ItemDex
-values(11, 'Dome Fossil', 'Changes into Kabuto.');
+values('Dome Fossil', 'Changes into Kabuto.');
 
 insert into ItemDex
-values(12, 'Elixer', 'Gives 10 PP to all Attacks.');
+values('Elixer', 'Gives 0 PP to all Attacks.');
 
 insert into ItemDex
-values(13, 'Escape Rope', 'Used to escape an area.');
+values('Escape Rope', 'Used to escape an area.');
 
 insert into ItemDex
-values(14, 'Ether', 'Gives 10 PP to one Ability.');
+values('Ether', 'Gives 0 PP to one Ability.');
 
 insert into ItemDex
-values(15, 'Exp. All', 'Shares experience points.');
+values('Exp. All', 'Shares experience points.');
 
 insert into ItemDex
-values(16, 'Fire Stone', 'Evolves some Fire Pokemon.');
+values('Fire Stone', 'Evolves some Fire Pokemon.');
 
 insert into ItemDex
-values(17, 'Fresh Water', 'Quenches thirst. Restores 50 HP.');
+values('Fresh Water', 'Quenches thirst. Restores 0 HP.');
 
 insert into ItemDex
-values(18, 'Full Heal', 'Cures any ailments.');
+values('Full Heal', 'Cures any ailments.');
 
 insert into ItemDex
-values(19, 'Full Restore', 'Cures any ailments. Restores all HP.');
+values('Full Restore', 'Cures any ailments. Restores all HP.');
 
 insert into ItemDex
-values(20, 'Gold Teeth', 'Helps Warden to Speak.');
+values('Gold Teeth', 'Helps Warden to Speak.');
 
 insert into ItemDex
-values(21, 'Good Rod', 'Used to fish.');
+values('Good Rod', 'Used to fish.');
 
 insert into ItemDex
-values(22, 'Great Ball', 'More effective than a Pokeball.');
+values('Great Ball', 'More effective than a Pokeball.');
 
 insert into ItemDex
-values(23, 'Guard Spec.', 'Disables Special Attacks.');
+values('Guard Spec.', 'Disables Special Attacks.');
 
 insert into ItemDex
-values(24, 'Helix Fossil', 'Changes into Omanyte.');
+values('Helix Fossil', 'Changes into Omanyte.');
 
 insert into ItemDex
-values(25, 'HP Up', 'Increases HP by 1.');
+values('HP Up', 'Increases HP by .');
 
 insert into ItemDex
-values(26, 'Hyper Potion', 'Restores 200 HP.');
+values('Hyper Potion', 'Restores 00 HP.');
 
 insert into ItemDex
-values(27, 'Ice Heal', 'Heals frozen Pokemon.');
+values('Ice Heal', 'Heals frozen Pokemon.');
 
 insert into ItemDex
-values(28, 'Iron', 'Increases Defence level.');
+values('Iron', 'Increases Defence level.');
 
 insert into ItemDex
-values(29, 'Item Finder', 'Finds Hidden Items.');
+values('Item Finder', 'Finds Hidden Items.');
 
 insert into ItemDex
-values(30, 'Leaf Stone', 'Evolves some Grass Pokemon');
+values('Leaf Stone', 'Evolves some Grass Pokemon');
 
 insert into ItemDex
-values(31, 'Lemonade', 'Quenches thirst. Restores 80 HP.');
+values('Lemonade', 'Quenches thirst. Restores 80 HP.');
 
 insert into ItemDex
-values(32, 'Lift Key', 'Activates Game Corner Elevator.');
+values('Lift Key', 'Activates Game Corner Elevator.');
 
 insert into ItemDex
-values(33, 'Master Ball', 'Catches Pokemon 100% of time.');
+values('Master Ball', 'Catches Pokemon 00% of time.');
 
 insert into ItemDex
-values(34, 'Max Elixer', 'Restores all PP.');
+values('Max Elixer', 'Restores all PP.');
 
 insert into ItemDex
-values(35, 'Max Ether', 'Restores all PP of 1 ability.');
+values('Max Ether', 'Restores all PP of  ability.');
 
 insert into ItemDex
-values(36, 'Max Potion', 'Restores all HP.');
+values('Max Potion', 'Restores all HP.');
 
 insert into ItemDex
-values(37, 'Max Repel', 'Prevents wild Pokemon from attacking.');
+values('Max Repel', 'Prevents wild Pokemon from attacking.');
 
 insert into ItemDex
-values(38, 'Max Revive', 'Revives. Restores all HP.');
+values('Max Revive', 'Revives. Restores all HP.');
 
 insert into ItemDex
-values(39, 'Moon Stone', 'Evolves some Pokemon.');
+values('Moon Stone', 'Evolves some Pokemon.');
 
 insert into ItemDex
-values(40, 'Nugget', 'Sold for $5000.');
+values('Nugget', 'Sold for $000.');
 
 insert into ItemDex
-values(41, 'Oaks Parcel', 'Exchange for Pokedex.');
+values('Oaks Parcel', 'Exchange for Pokedex.');
 
 insert into ItemDex
-values(42, 'Old Amber', 'Changes into Aerodactyl.');
+values('Old Amber', 'Changes into Aerodactyl.');
 
 insert into ItemDex
-values(43, 'Old Rod', 'Fish for Magikarp.');
+values('Old Rod', 'Fish for Magikarp.');
 
 insert into ItemDex
-values(44, 'Paralyze Heal', 'Heals Paralysis.');
+values('Paralyze Heal', 'Heals Paralysis.');
 
 insert into ItemDex
-values(45, 'Pokeball', 'Lowest odds of catching Pokemon.');
+values('Pokeball', 'Lowest odds of catching Pokemon.');
 
 insert into ItemDex
-values(46, 'Pokedex', 'Records Pokemon Data.');
+values('Pokedex', 'Records Pokemon Data.');
 
 insert into ItemDex
-values(47, 'Pokedoll', 'Distracts Opponents. Makes girls happy.');
+values('Pokedoll', 'Distracts Opponents. Makes girls happy.');
 
 insert into ItemDex
-values(48, 'Pokeflute', 'Wakes sleeping Pokemon.');
+values('Pokeflute', 'Wakes sleeping Pokemon.');
 
 insert into ItemDex
-values(49, 'Potion', 'Restores 20 HP.');
+values('Potion', 'Restores 0 HP.');
 
 insert into ItemDex
-values(50, 'PP Up', 'Increases PP by 1.');
+values('PP Up', 'Increases PP by .');
 
 insert into ItemDex
-values(51, 'Protein', 'Increases Attack level.');
+values('Protein', 'Increases Attack level.');
 
 insert into ItemDex
-values(52, 'Rare Candy', 'Increases Exp Level by 1.');
+values('Rare Candy', 'Increases Exp Level by .');
 
 insert into ItemDex
-values(53, 'Repel', 'Prevents wild Pokemon from attacking.');
+values('Repel', 'Prevents wild Pokemon from attacking.');
 
 insert into ItemDex
-values(54, 'Revive', 'Revives fainted Pokemon.');
+values('Revive', 'Revives fainted Pokemon.');
 
 insert into ItemDex
-values(55, 'S.S. Ticket', 'Used to Board S.S. Anne.');
+values('S.S. Ticket', 'Used to Board S.S. Anne.');
 
 insert into ItemDex
-values(56, 'Safari Ball', 'Used in Safari Zone.');
+values('Safari Ball', 'Used in Safari Zone.');
 
 insert into ItemDex
-values(57, 'Secret Key', 'Unlocks Cinnabar Island Gym.');
+values('Secret Key', 'Unlocks Cinnabar Island Gym.');
 
 insert into ItemDex
-values(58, 'Silph Scope', 'Used to identify Ghosts.');
+values('Silph Scope', 'Used to identify Ghosts.');
 
 insert into ItemDex
-values(59, 'Soda Pop', 'Quenches thirst. Restores 60 HP.');
+values('Soda Pop', 'Quenches thirst. Restores 0 HP.');
 
 insert into ItemDex
-values(60, 'Super Potion', 'Restores 50 HP.');
+values('Super Potion', 'Restores 0 HP.');
 
 insert into ItemDex
-values(61, 'Super Repel', 'Prevents wild Pokemon from attacking.');
+values('Super Repel', 'Prevents wild Pokemon from attacking.');
 
 insert into ItemDex
-values(62, 'Super Rod', 'Fishes for Pokemon.');
+values('Super Rod', 'Fishes for Pokemon.');
 
 insert into ItemDex
-values(63, 'Thunder Stone', 'Evolves Pikachu.');
+values('Thunder Stone', 'Evolves Pikachu.');
 
 insert into ItemDex
-values(64, 'Town Map', 'Shows you where you are.');
+values('Town Map', 'Shows you where you are.');
 
 insert into ItemDex
-values(65, 'Ultra Ball', 'Better than Great Balls.');
+values('Ultra Ball', 'Better than Great Balls.');
 
 insert into ItemDex
-values(66, 'Water Stone', 'Evolves some water Pokemon.');
+values('Water Stone', 'Evolves some water Pokemon.');
 
 insert into ItemDex
-values(67, 'X Accuracy', 'Boosts Accuracy Temporarily.');
+values('X Accuracy', 'Boosts Accuracy Temporarily.');
 
 insert into ItemDex
-values(68, 'X Attack', 'Boosts Attack Temporarily.');
+values('X Attack', 'Boosts Attack Temporarily.');
 
 insert into ItemDex
-values(69, 'X Defend', 'Boosts Defence Temporarily.');
+values('X Defend', 'Boosts Defence Temporarily.');
 
 insert into ItemDex
-values(70, 'X Special', 'Boosts Special Temporarily.');
+values('X Special', 'Boosts Special Temporarily.');
 
 insert into ItemDex
-values(71, 'X Speed', 'Boosts Speed Temporarily.');
+values('X Speed', 'Boosts Speed Temporarily.');
