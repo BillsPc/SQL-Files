11719abf2ec84ad3d79dc1bb06f39941d214156a
Josh Foo <foojoshua2007@gmail.com>
2014-06-13 17:49:14 -0700

    Pokemon Gender Fix
    Fixed the gender for random pokemon to be one of M,F or U.

--- pokemon.sql
+++ pokemon.sql
@@ -1,334 +1,339 @@
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 insert into Pokemon(pokeName, dexNo, level, gender)
-values ('1', Rand() * 151, Rand() * 100, 'M');
+values ('1', Rand() * 151, Rand() * 100, '2');
 
 UPDATE Pokemon
 INNER JOIN Pokedex ON Pokemon.dexNo = Pokedex.dexNo
 SET Pokemon.pokeName=Pokedex.species
 WHERE Pokemon.pokeName = '1';
+
+UPDATE Pokemon
+INNER JOIN Pokedex ON Pokemon.dexNo = Pokedex.dexNo
+SET Pokemon.gender= SUBSTRING(Pokedex.gender, 1 + FLOOR(RAND()*LENGTH(Pokedex.gender)), 1)
+WHERE Pokemon.gender = '2';
